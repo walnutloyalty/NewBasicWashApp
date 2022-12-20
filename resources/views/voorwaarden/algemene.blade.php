be13bf58751609c8eadf6fe9fc1cb238b49dbb37
HamzaElbarkani <hamzagdf39@gmail.com>
2022-12-20 11:14:46 +0100

    Update algemene.blade.php
    Algemene voorwaarden afgemaakt

--- resources/views/voorwaarden/algemene.blade.php
+++ resources/views/voorwaarden/algemene.blade.php
@@ -49,15 +49,197 @@
                 </ul>
                 <!--Artikel 3-->
                 <h2 class="text-xl">Artikel 3 Uitleg gebruik</h2>
-                <p>Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>. Amet, massa quam varius orci
-                    dapibus volutpat cras. In amet eu ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
-                    viverra ridiculus non molestie. Gravida quis fringilla amet eget dui tempor dignissim. Facilisis
-                    auctor venenatis varius nunc, congue erat ac. Cras fermentum convallis quam.</p>
-                <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed
-                    amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius
-                    sit neque erat velit.</p>
+                <p>Cliënt dient zich voorafgaand aan het gebruik van de Autowasstraat te informeren naar het juiste
+                    gebruik ervan.
+                </p>
+                <!--Artikel 4-->
+                <h2 class="text-xl">Artikel 4 Instructies</h2>
+                <p>Cliënt dient zich te houden aan de instructies op de instructieborden bij de ingang van de
+                    Autowasstraat en dient de aanwijzingen op te volgen die door (een medewerker) van het Autowasbedrijf
+                    mondeling of middels gebaren worden gegeven. Daarnaast mag het Autowasbedrijf ervan uitgaan dat
+                    Cliënt handelt volgens algemeen bekende gebruiksnormen in een wasstraat.
+                </p>
+                <!--Artikel 5-->
+                <h2 class="text-xl">Artikel 5 Geschiktheid van het voertuig</h2>
+                <p>Het voertuig van Cliënt is alleen geschikt voor de Autowasstraat als het voldoet aan de volgende
+                    eisen:</p>
+                <ul class="list-disc">
+                    <li>Het voertuig heeft vier wielen en een dichte carrosserie; slechts indien uitdrukkelijk door het
+                        Autowasbedrijf is aangegeven dat de wasinstallatie tevens geschikt is voor bijvoorbeeld met een
+                        soft top of minder dan 4 wielen kan van het voorgaande worden afgeweken.
+                    </li>
+                    <li>De op de Autowas- en poetsstraat aangegeven maximum-afmetingen (hoogtebreedte-lengte) worden
+                        niet overschreden.
+                    </li>
+                    <li>
+                        Het voertuig van de Cliënt mag geen loszittende en/of uitstekende onderdelen hebben die tijdens
+                        de verkregen dienst losgerukt kunnen worden. Onderdelen die los kunnen zitten en/of uitsteken
+                        zijn o.a. antennes, spiegels, siertrips, stootlijsten, bumpers, spoilers, schijnwerpers en
+                        ruiten- en koplampwissers; ook niet originele onderdelen of wijzigingen van de carrosserie
+                        kunnen loszitten. Door slijtage, kleine schaden, ouderdom etc. kunnen onderdelen die vast lijken
+                        te zitten, toch loszitten.
+                    </li>
+                    <li>
+                        Cliënt dient zelf na te gaan of diens voertuig geschikt is en dient in geval van twijfel een
+                        medewerker van het Autowasbedrijf om advies te vragen zonder dat deze medewerker de
+                        verantwoordelijkheid van de Cliënt overneemt. Cliënt dient bijzonderheden (bv. modificaties of
+                        meerdere schaden) voorafgaand aan de was- of poetsbehandeling bij een medewerker van het
+                        Autowasbedrijf te melden.
+                    </li>
+                </ul>
+                <!--Artikel 6-->
+                <h2 class="text-xl">Artikel 6 Wasresultaat</h2>
+                <p>Bij een normaal vervuilde auto mag Cliënt verwachten dat het in water oplosbaar vuil wordt
+                    verwijderd. Er wordt een voorbehoud gemaakt ten aanzien van de mogelijkheid bepaalde vervuiling te
+                    verwijderen. Cliënt mag niet verwachten dat vet, teer en krassen etc. worden verwijderd.</p>
+                <!--Artikel 7-->
+                <h2 class="text-xl">Artikel 7 Wasboxen</h2>
+                <p>Het gebruik van zogenoemde wasboxen van het Autowasbedrijf is geheel voor risico van Cliënt. Het
+                    Autowasbedrijf is niet aansprakelijk voor schade aan het voertuig van Cliënt die is ontstaan door
+                    het gebruik van de wasbox.</p>
+                <!--Artikel 8-->
+                <h2 class="text-xl">Artikel 8 Randapparatuur</h2>
+                <p>Het gebruik van zogenoemde randapparatuur voor reiniging van de auto van het Autowasbedrijf is geheel
+                    voor risico van Cliënt. Het Autowasbedrijf is niet aansprakelijk voor schade aan het voertuig van
+                    Cliënt die is ontstaan door het gebruik van de randapparatuur.
+                    Onder randapparatuur voor reiniging van de auto wordt onder andere verstaan; Stofzuigers,
+                    matten-wassers/reinigers, interieurreinigers, luchtklokken, dashboardguns, verkoopautomaten of
+                    vergelijkbare apparatuur. Het Auto- en poetsbedrijf is niet aansprakelijk voor diefstal of verlies
+                    van zaken (waaronder lading, inventaris, schriftelijke bescheiden en waardepapieren) van Cliënt
+                    en/of derden die zich in het voertuig bevinden. Cliënt is ook zelf verantwoordelijk voor eventueel
+                    loszittende delen van het dashboard en andere binnenpanelen van het voertuig.</p>
+                <!--Artikel 9-->
+                <h2 class="text-xl">Artikel 9 Klachten</h2>
+                <p>In geval van een klacht of indien het wasresultaat niet is zoals Cliënt redelijkerwijs mocht
+                    verwachten, dient de Cliënt een medewerker van het Autowasbedrijf onverwijld aan te spreken. Wordt
+                    de klacht aangaande het wasresultaat door het Autowasbedrijf gegrond verklaard, dan heeft Cliënt
+                    recht op éénmaal opnieuw wassen met gebruikmaking van een wasprogramma dat door het Autowasbedrijf
+                    wordt gekozen.</p>
+                <!--Artikel 10-->
+                <h2 class="text-xl">Artikel 10 Schade</h2>
+                <ul class="list-disc">
+                    <li>Het Autowasbedrijf is niet aansprakelijk voor indirecte schade, waaronder begrepen maar niet
+                        beperkt tot gevolgschade of gederfde winst.
+                    </li>
+                    <li>Het Autowasbedrijf is slechts aansprakelijk voor directe schade die ontstaat door aantoonbare
+                        fouten van haar medewerkers, gebruik van ongeschikt (was)middel of gebreken van de installatie.
+                        Er geldt echter een maximum van € 4500,00 exclusief btw behoudens opzet of bewuste
+                        roekeloosheid.
+                    </li>
+                    <li>Cliënt is aansprakelijk voor schade bij eigen voertuig, bij het Autowasbedrijf of bij andere
+                        gebruikers, wanneer Cliënt instructies niet opvolgt en/of een ongeschikte dienst laat uitvoeren
+                        aan zijn voertuig
+                    </li>
+                    <li>
+                        Schade, waarvoor cliënt meent dat het Autowasbedrijf aansprakelijk is, dient voor het verlaten
+                        van het bedrijfsterrein van de het Autowasbedrijf, maar in ieder geval binnen een termijn van 24
+                        uur aan een medewerker van het Autowasbedrijf te worden gemeld.
+                    </li>
+                    <li>
+                        Het Autowasbedrijf honoreert bij vermeende aansprakelijkheid van het Autowasbedrijf geen verzoek
+                        tot vergoeding van reparaties en herstellingen die zijn uitgevoerd zonder overleg en
+                        uitdrukkelijke goedkeuring vooraf van het Autowasbedrijf.
+                    </li>
+                    <li>
+                        Indien Cliënt van mening is dat het Autowasbedrijf aansprakelijk is, kan hij zich (na zich eerst
+                        tot het Autowasbedrijf te hebben gericht), wenden tot een door het Autowasbedrijf aangewezen
+                        schade expertisebedrijf. Partijen kunnen vervolgens, op kosten van ongelijk, aan het schade
+                        expertisebedrijf een oordeel vragen over de oorzaak van het vermeende schadebeeld en over de
+                        aansprakelijkheid van het Autowasbedrijf. Cliënt en het Autowasbedrijf volgen de conclusie van
+                        deze schaderapportage op. Dit schaderapport zal bindend zijn, tenzij de meeste gerede partij
+                        binnen twee maanden na overlegging van het rapport naar de rechter is gegaan, om daar een
+                        oordeel over vermeende aansprakelijkheid van het Autowasbedrijf te vragen.
+                    </li>
+                </ul>
+                <!--Artikel 11-->
+                <h2 class="text-xl">Artikel 11 Overmacht</h2>
+                <p>In geval van overmacht is het Autowasbedrijf niet aansprakelijk voor directe schade. Onder overmacht
+                    wordt in ieder geval verstaan: een tekortkoming die het Autowasbedrijf niet kan worden toegerekend
+                    doordat zij niet te wijten is aan haar schuld, noch krachtens wet, rechtshandeling of naar in het
+                    maatschappelijk verkeer geldende opvattingen voor haar rekening komt, waaronder begrepen het geval
+                    dat het Autowasbedrijf door een (toerekenbare) tekortkoming of onzorgvuldigheid van derden niet in
+                    staat is haar diensten te verrichten.</p>
+
+                <!--Artikel 12-->
+                <h2 class="text-xl">Artikel 12 Abonnementen</h2>
+                <ul class="list-disc">
+                    <li>Sommige Autowasstraten van het Autowasbedrijf bieden ook (of enkel) abonnementsvormen aan.
+                        Voorwaarden voor abonnementen zijn opgenomen in de Algemene Abonnementsvoorwaarden van Berkman
+                        Carwash B.V., deze zijn te vinden op de website van het Autowasbedrijf. Daarnaast kan Cliënt te
+                        allen tijde een (digitale) kopie van de Abonnementsvoorwaarden opvragen bij het Autowasbedrijf.
+                    </li>
+                    <li>Voor autowassen met abonnementen geldt het normaal consumenten gebruik, hier dient de Cliënt
+                        zich aan te houden. Onder normaal consumenten gebruik (fair use policy) wordt verstaan dat het
+                        gebruik van de Autowasstraat niet hoger mag liggen dan drie maal het gemiddelde gebruik van het
+                        desbetreffende abonnement.
+                    </li>
+                </ul>
+                <!--Artikel 13-->
+                <h2 class="text-xl">Artikel 13 Weigeren van klanten</h2>
+                <p>Het Autowasbedrijf kan een
+                    (potentiële) Cliënt weigeren indien zijn voertuig voor de autowasinstallatie ongeschikt is of
+                    als het Autowasbedrijf daarvoor een andere reden gegrond acht.</p>
+                <!--Artikel 14-->
+                <h2 class="text-xl">Artikel 14 Persoonsgegevens</h2>
+                <ul class="list-disc">
+                    <li>Cliënt gaat bij acceptatie van offerte en/of bij het sluiten van een overeenkomst akkoord met de
+                        verwerking van zijn persoonsgegevens door het Autowasbedrijf.
+                    </li>
+                    <li>
+                        De persoonsgegevens van Cliënt die worden vermeld op enig formulier (waaronder o.a. het
+                        aanmeldingsformulier) worden door het Autowasbedrijf verwerkt, mogelijk in de zin van de wet op
+                        Bescherming van Persoonsgegevens (Wbp).
+                    </li>
+                    <li>
+                        Aan de hand van deze verwerking kan het Autowasbedrijf: de overeenkomst uitvoeren en zijn
+                        verplichtingen jegens cliënt nakomen, Cliënt een optimale service verlenen, hen tijdig voorzien
+                        van actuele was-informatie en hem gepersonaliseerde aanbiedingen doen. Daarnaast kunnen de
+                        persoonsgegevens beschikbaar worden gesteld ten behoeve van direct marketingactiviteiten.
+                    </li>
+                    <li>
+                        Tegen verwerking van persoonsgegevens in de zin van de Wbp ten behoeve van direct
+                        mailingactiviteiten wordt het eventueel door Cliënt bij het Autowasbedrijf aan te tekenen verzet
+                        gehonoreerd.
+                    </li>
+                    <li>
+                        Persoonsgegevens worden onomkeerbaar verwijderd wanneer zij niet meer noodzakelijk zijn, tenzij
+                        bewaren daarna verplicht is op grond van de wet.
+                    </li>
+                </ul>
+                <!--Artikel 15-->
+                <h2 class="text-xl">Artikel 15 Toepasselijk recht</h2>
+                <p>Op deze voorwaarden en overeenkomsten waarop de voorwaarden geheel of gedeeltelijk betrekking hebben,
+                    evenals op geschillen die uit deze voorwaarden voortvloeien of daarmee verband houden, is
+                    uitsluitend Nederlands recht van toepassing.</p>
+                <!--Artikel 16-->
+                <h2 class="text-xl">Artikel 16 Geschillen</h2>
+                <ul class="list-disc">
+                    <li>
+                        Overmatig gebruik van de Autowasstraat: wanneer Cliënt overmatig gebruik maakt van een
+                        Autowasstraat van het Autowasbedrijf, zal de Cliënt hiervan per e-mail op de hoogte worden
+                        gesteld. Onder overmatig gebruik wordt het schenden van de ‘fair use policy’ (artikel 12.2)
+                        verstaan. Mocht de Cliënt na berichtgeving geen verandering in zijn/haar wasfrequentie tonen,
+                        kan het Autowasbedrijf deze Cliënt weigeren in al haar Autowasstraten.
+                    </li>
+                    <li>
+                        Wanbetaling(en): wanneer Cliënt een betaling aan het Autowasbedrijf niet voldoet (bv. wegens
+                        onvoldoende saldo of storneren van een automatische incasso), zal de Cliënt hiervan per e-mail
+                        door het Autowasbedrijf op de hoogte worden gesteld. Tegelijkertijd wordt de Cliënt per direct
+                        geblokkeerd tot de betaling is ontvangen. Dit houdt niet in dat het abonnement stop wordt gezet.
+                        Eerdergenoemde berichtgeving bevat tevens een betaalverzoek, zodat de achterstallige
+                        betaling(en) direct kunnen worden voldaan. Na uitblijven van reactie en/of betaling wordt er een
+                        tweede e-mail naar de Cliënt gestuurd. Cliënt wordt gebeld (of een derde keer gemaild als er
+                        geen telefoonnummer bekend is) indien er wederom geen respons is. Mocht reactie en/of betaling
+                        nog steeds uitblijven, dan behoudt het Autowasbedrijf het recht om de Cliënt aan te melden bij
+                        haar incassopartner.
+                    </li>
+                    <li>
+                        Geschillen naar aanleiding van rechtshandelingen, offertes en overeenkomsten waarop deze
+                        voorwaarden van toepassing zijn of naar aanleiding van nadere overeenkomsten die daaruit
+                        voortvloeien, zullen worden beslecht door de Rechtbank Rotterdam.
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
-
 </x-app-layout>
