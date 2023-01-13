19d20e015ff37ec282293f140246dfb078ad4b39
Hamza <hamza.elbarkani@edu-kw1c.nl>
2023-01-13 09:43:05 +0100

    Update abonnements.blade.php

--- resources/views/voorwaarden/abonnements.blade.php
+++ resources/views/voorwaarden/abonnements.blade.php
@@ -82,6 +82,116 @@
                     zich het recht voor de voormalig Abonnee te weigeren een volgend Abonnement af te sluiten.</p>
                 <!--Artikel 5-->
                 <h2 class="mt-8 font-bold text-xl leading-8 text-black">Artikel 5 Gebruik van het abonnement</h2>
+                <p>De Abonnee heeft recht om één voertuig onbeperkt te wassen bij de Wasstraat van Berkman Carwash B.V.,
+                    mits sprake is van normaal consumenten gebruik zoals gedefinieerd in Artikel 5.2.</p>
+                <p>Onder normaal consumenten gebruik (fair use policy) wordt verstaan dat het gebruik van de Wasstraat
+                    niet hoger mag liggen dan drie maal het gemiddelde gebruik van het desbetreffende Abonnement.</p>
+                <!--Artikel 6-->
+                <h2 class="mt-8 font-bold text-xl leading-8 text-black">Artikel 6 Prijzen</h2>
+                <p>Berkman Carwash B.V. behoudt zich het recht voor de prijs van het Abonnement te wijzigen. Berkman
+                    Carwash B.V. stelt de Abonnee hiervan minimaal één maand voordat de prijswijziging van kracht wordt
+                    per e-mail op de hoogte.</p>
+                <p>Alle genoemde prijzen zijn onder voorbehoud van druk- en zetfouten. Voor de gevolgen van druk- en
+                    zetfouten wordt geen aansprakelijkheid aanvaard.</p>
+                <p>Alle genoemde prijzen zijn in Euro’s en inclusief BTW.</p>
+                <p>Restitutie van het Abonnementsgeld is niet mogelijk, ook niet bij storing, weersomstandigheden of
+                    enige andere oorzaak van tijdelijke sluiting van de Wasstraat.</p>
+                <!--Artikel 7-->
+                <h2 class="mt-8 font-bold text-xl leading-8 text-black">Artikel 7 Betaling</h2>
+                <p>Voor een maandabonnement is het abonnementsgeld maandelijks verschuldigd. Dit wordt maandelijks via
+                    automatische incasso afgeschreven van de door Abonnee opgegeven bankrekening. De eerste termijn
+                    dient direct via iDEAL te worden betaald. Bij een niet tijdige betaling van het abonnementsgeld is
+                    Berkman Carwash B.V. genoodzaakt € 15,00 administratiekosten in rekening te brengen.</p>
+                <p>Voor een jaarabonnement dient de eerste termijn direct via iDEAL te worden betaald. Dit wordt
+                    jaarlijks via automatische incasso afgeschreven van de door Abonnee opgegeven bankrekening. Bij een
+                    niet tijdige betaling van het abonnementsgeld is Berkman Carwash B.V. genoodzaakt € 15,00
+                    administratiekosten in rekening te brengen.</p>
+                <p>Door ondertekening van de abonnementsovereenkomst machtigt de Abonnee
+                    Berkman Carwash B.V., dan wel een door Berkman Carwash B.V. aan te wijzen derde, tot incasso van het
+                    abonnementsgeld.</p>
+                <p>Maandelijkse abonnementskosten en eventuele kosten in verband met het gebruik van het Abonnement
+                    worden maandelijks afgeschreven op het door Abonnee opgegeven rekeningnummer op de kalenderdag
+                    waarop het betalende gedeelte van je lidmaatschap is ingegaan. In sommige gevallen kan de
+                    betalingsdatum worden gewijzigd, bijvoorbeeld als Berkman Carwash B.V. een bedrag niet kon
+                    afschrijven of als je betaalde lidmaatschap is ingegaan op een dag die in een bepaalde maand niet
+                    voorkomt.</p>
+                <p>Het is niet mogelijk de betaling van het abonnementsgeld aan de kassa van de Wasstraat te
+                    voldoen.</p>
+                <p>Indien de incasso, om wat voor reden dan ook, storneert, behoudt Berkman Carwash B.V. zich het recht
+                    voor haar verplichtingen onder het Abonnement op te schorten en het Abonnement buiten werking te
+                    stellen. Het Abonnement wordt weer in gebruik gesteld zodra de Abonnee het volledig verschuldigde
+                    abonnementsgeld en/of eventuele administratiekosten heeft voldaan. De Abonnee heeft geen recht op
+                    restitutie van abonnementsgeld over de periode dat het Abonnement buiten werking is geweest.</p>
+                <p>Indien de Abonnee niet tijdig voldoet aan de betalingsverplichtingen voortkomend uit deze
+                    voorwaarden, zal Berkman Carwash B.V. de daardoor ontstane incassokosten en eventuele rentekosten
+                    verhalen op Abonnee. Tevens zal Berkman Carwash B.V. de betalingsvordering overdragen aan een door
+                    haar aan te wijzen incassobureau. De kosten die Berkman Carwash B.V. redelijkerwijs zal moeten maken
+                    als gevolg van deze niet nakoming van de Abonnee, komen voor rekening van de Abonnee.</p>
+                <p>Op een jaarabonnement na, voldoet de Abonnee alle betalingen in enkelvoudige maandelijkse termijnen.
+                    Afwijkende bedragen zullen worden teruggestort en zullen niet kwalificeren als geldende
+                    betaling.</p>
+                <p>Wijziging van het kenteken, bankrekeningnummer en/of te naam stelling zijn enkel mogelijk middels het
+                    afsluiten van een nieuw Abonnement zoals beschreven in Artikel 8.</p>
+                <!--Artikel 8-->
+                <h2 class="mt-8 font-bold text-xl leading-8 text-black">Artikel 8 Wijziging abonnementsgegevens</h2>
+                <p>Het bij een Abonnement opgegeven kenteken kan niet door Abonnee zelf worden gewijzigd. Abonnee kan
+                    contact opnemen met de klantenservice van Berkman Carwash B.V. via info@basicwash.nl om een kenteken
+                    te wijzigen.</p>
+                <p>Bankrekeningnummer en te naam stelling kunnen niet worden aangepast in een
+                    reeds afgesloten Abonnement.</p>
+                <p>Abonnee kan zijn/haar bankrekeningnummer en/of te naam stelling wijzigen door het huidige Abonnement
+                    te beëindigen, waarna een Abonnement met de nieuwe gegevens moet worden afgesloten.</p>
+                <p>De Abonnee heeft geen recht op restitutie van abonnementsgeld over de periode dat het Abonnement niet
+                    kan worden gebruikt.</p>
+                <!--Artikel 9-->
+                <h2 class="mt-8 font-bold text-xl leading-8 text-black">Artikel 9 Klantenservice</h2>
+                <p>Voor vragen over deze Abonnementsvoorwaarden, het gebruik van het Abonnement en het opzeggen van een
+                    Abonnement kan men terecht bij de Afdeling Abonnementen. Deze is bereikbaar via e-mailadres <a
+                        href="mailto:info@basicwash.nl" class="text-pink-600 no-underline">info@basicwash.nl</a>, via
+                    telefoonnummer 0180 – 643 643 (bereikbaar op ma t/m vr van 09.00 – 17.00 uur, feestdagen gesloten)
+                    of via een schrijven aan: Berkman Carwash B.V., t.a.v. Afdeling Abonnementen, Tuindersweg 34, 2991
+                    LR Barendrecht.</p>
+                <!--Artikel 10-->
+                <h2 class="mt-8 font-bold text-xl leading-8 text-black">Artikel 10 Klantgegevens</h2>
+                <p>De Abonnee houdt Berkman Carwash B.V. onverwijld op de hoogte van een wijziging van het
+                    e-mailadres.</p>
+                <p>Bij het verwerken van persoonsgegevens houdt Berkman Carwash B.V. zich aan de geldende regelgeving,
+                    waaronder de Wet bescherming persoonsgegevens. Berkman Carwash B.V. zal persoonsgegevens niet
+                    verstrekken aan derden.</p>
+                <p>Berkman Carwash B.V. zal het mailadres van de Abonnee gebruiken om de Abonnee te informeren over
+                    verschillende zaken, waaronder gepland onderhoud. Ook voor zaken met hoge spoed zal Berkman Carwash
+                    B.V. contact opnemen met Abonnee via het opgegeven e-mailadres.</p>
+                <!--Artikel 11-->
+                <h2 class="mt-8 font-bold text-xl leading-8 text-black">Artikel 11 Inschakelen derden</h2>
+                <p>Berkman Carwash B.V. heeft het recht om bij de uitvoering van het Abonnement
+                    derden in te schakelen.</p>
+                <p>Het is Abonnee niet toegestaan om het Abonnement over te dragen aan derden zonder voorafgaande
+                    schriftelijke toestemming van Berkman Carwash B.V.</p>
+                <!--Artikel 12-->
+                <h2 class="mt-8 font-bold text-xl leading-8 text-black">Artikel 12 Gratis proefperioden</h2>
+                <p>Een Abonnement kan met een gratis proefperiode beginnen. De gratis proefperiode is bedoeld om nieuwe
+                    leden en bepaalde voormalige Abonnees de service uit te laten proberen.</p>
+                <p>Berkman Carwash B.V. bepaalt naar eigen inzicht of je in aanmerking komt voor de gratis proefperiode
+                    en kan de criteria hiervoor beperken om te voorkomen dat gratis proefperioden worden misbruikt.
+                    Berkman Carwash B.V. behoudt zich het recht voor om de gratis proefperiode in te trekken als zij
+                    vaststelt dat je hiervoor niet in aanmerking komt. Aan de hand van gegevens die voor een bestaand of
+                    recent Abonnement worden gebruikt, zoals een kenteken, betalingsmethode of e-mailadres voor je
+                    account, kan Berkman Carwash B.V. bepalen of je in aanmerking komt voor de gratis proefperiode. Er
+                    kunnen beperking gelden voor combinaties met andere aanbiedingen.</p>
+                <p>Berkman Carwash B.V. schrijft je maandelijkse abonnementskosten aan het einde van de gratis
+                    proefperiode af via je betaalmethode, tenzij je je Abonnement voor het einde van de gratis
+                    proefperiode opzegt.</p>
+                <!--Artikel 13-->
+                <h2 class="mt-8 font-bold text-xl leading-8 text-black">Artikel 13 Overige bepalingen en toepasselijk
+                    recht</h2>
+                <p>Op alle rechtsverhoudingen tussen Berkman Carwash B.V. en de Abonnee is Nederlands recht van
+                    toepassing.</p>
+                <p>Indien enige bepaling van deze Algemene Voorwaarden om wat voor reden dan ook ongeldig of nietig
+                    mocht zijn dan wel mocht worden vernietigd, blijven de overige bepalingen onverminderd van kracht en
+                    zullen partijen in onderling overleg voorzien in een vervangende bepaling waarbij het doel en de
+                    strekking van de te vervangen bepaling zoveel mogelijk wordt behouden.</p>
+                <p>Berkman Carwash B.V. is niet aansprakelijk voor elke vorm van letsel of schade aan zowel de auto als
+                    de Abonnee door onjuist gebruik van de wasinstallatie.</p>
             </div>
         </div>
     </div>
