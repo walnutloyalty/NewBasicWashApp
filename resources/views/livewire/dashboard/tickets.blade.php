dab597ebca614faa2466f2b8e9d739fad0931e25
WouterLagerwerf <43637154+WouterLagerwerf@users.noreply.github.com>
2023-03-10 16:28:04 +0100

    Update tickets.blade.php

--- resources/views/livewire/dashboard/tickets.blade.php
+++ resources/views/livewire/dashboard/tickets.blade.php
@@ -69,13 +69,6 @@
                 class="relative flex min-h-screen items-center justify-center p-4">
                 <div x-on:click.stop x-trap.noscroll.inert="open"
                     class="relative w-full max-w-2xl overflow-y-auto rounded-xl bg-white p-4 shadow-lg">
-                    <div class="text-center mx-auto h-12 w-12 text-pink-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="mx-auto h-12 w-12">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z" />
-                        </svg>
-                    </div>
                     <div class="mt-3 text-center sm:mt-5">
                         <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">{{ __('Status') }}
                         </h3>
