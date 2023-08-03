e3170c30596232020742e178f9d1d0b9c1ecb9ff
WouterLagerwerf <43637154+WouterLagerwerf@users.noreply.github.com>
2023-08-03 11:37:54 +0200

    Update pricing.blade.php

--- resources/views/livewire/pricing.blade.php
+++ resources/views/livewire/pricing.blade.php
@@ -1,7 +1,7 @@
 <div x-data="{ private: true }" class="@if($iframe) bg-white @else bg-gray-50 @endif">
     <div class="mx-auto max-w-7xl @if(! $iframe) py-24 @else py-6 @endif px-4 sm:px-6 lg:px-8">
         <div class="sm:align-center sm:flex sm:flex-col">
-            <h1 class="text-5xl font-bold tracking-tight text-gray-900 sm:text-center">{{__("Onze abbonementen")}}</h1>
+            <h1 class="text-5xl font-bold tracking-tight text-gray-900 sm:text-center">{{__("Onze abonnementen")}}</h1>
             <p class="mt-5 text-xl text-gray-500 sm:text-center">{{__("Goedkoop autowassen voor iedereen")}}
             </p>
             <div class="relative mt-6 flex self-center rounded-lg bg-gray-100 p-0.5 sm:mt-8">
