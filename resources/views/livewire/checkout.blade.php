ec488314ae1d2f1b31ad301174ba53cf8d66c811
onno204 <onno@walnutloyalty.com>
2023-10-24 17:22:42 +0200

    Added automatically applying the licenseplate

--- resources/views/livewire/checkout.blade.php
+++ resources/views/livewire/checkout.blade.php
@@ -134,9 +134,9 @@
                         <label class="sr-only">{{ __('Kenteken') }}</label>
                         <input type="text" name="userLicensePlate" wire:model="licenseplate" id="userLicensePlate"
                             class="block w-full rounded-md border-gray-300 shadow-sm focus:border-pink-500 focus:ring-pink-500 sm:text-sm"
-                            placeholder="Enter a licenseplate">
+                            placeholder="Enter a licenseplate" @keyup="if (document.getElementsByClassName('addLicenseplate').length == 0 && $event.target.value.replace('-', '').replace('-', '').length === 6) { document.getElementById('addLicensePlateButton').click() }">
                         <button type="button" wire:click="addLicensePlate" wire:loading.attr="disabled"
-                            wire:target="addLicensePlate"
+                            wire:target="addLicensePlate" id="addLicensePlateButton"
                             class="ml-4 flex-shrink-0 rounded-full border border-transparent bg-pink-600 p-2 text-sm font-medium text-white shadow-sm hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2">
                             <svg wire:target="addLicensePlate" wire:loading.remove xmlns="http://www.w3.org/2000/svg"
                                 fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
@@ -164,7 +164,7 @@
                     <ul role="list" class="mt-4 divide-y divide-gray-200 border-b border-t border-gray-200">
                         @foreach ($licenseplates as $key => $plate)
                             <li
-                                class="relative bg-white px-4 py-5 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 hover:bg-gray-50">
+                                class="addLicenseplate relative bg-white px-4 py-5 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 hover:bg-gray-50">
                                 <div class="flex w-full justify-between">
                                     <p class="truncate text-sm font-medium text-gray-900">{{ $plate }}</p>
                                     <p wire:click="removePlate({{ $key }})"
@@ -370,7 +370,7 @@
                                         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
                                     </path>
                                 </svg>
-                                <span x-text="nav_step === 2 ? 'Checkout' : 'Next'" wire:loading.remove wire:target="next">
+                                <span x-text="nav_step === 2 ? 'Checkout' : 'Next'" wire:loading.remove wire:target="next" @click="window.scrollTo(0,0)">
 
                                 </span>
                             </button>
--- resources/views/livewire/pricing.blade.php
+++ resources/views/livewire/pricing.blade.php
@@ -1,8 +1,8 @@
 <div x-data="{ private: {{ $private ? 'true' : 'false' }}, showTypeSelector: {{ $hideselector ? 'false' : 'true' }}, hideTypeSelector: {{ $hideselector ? 'true' : 'false' }}, nav_step: 0, checkout: false }"
-    @setnavstep.window="nav_step = $event.detail.step; console.log('navstep: ', nav_step, typeof nav_step, $event.detail.step)"
+    @setnavstep.window="nav_step = $event.detail.step; setTimeout(() => { window.scrollTo(0,0) }, 150); if(nav_step == 2){ setTimeout(() => { document.getElementById('userLicensePlate').focus() }, 200 ) } console.log('navstep: ', nav_step, typeof nav_step, $event.detail.step)"
     @setshowtypeselector.window="showTypeSelector = $event.detail.status"
     @setprivate.window="private = $event.detail.status"
-    class="@if ($iframe) bg-white @else bg-gray-50 @endif">
+    class="@if ($iframe) @else @endif bg-gray-50 bg-white">
     <div class="@if (!$iframe) py-24 @else py-6 @endif mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="sm:align-center sm:flex sm:flex-col">
             <h1 class="text-5xl font-bold tracking-tight text-gray-900 sm:text-center">{{ __('Onze abonnementen') }}</h1>
